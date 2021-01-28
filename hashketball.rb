4d300665f206a38847f844432e33750dbab3e4ec
Destiny Boling <bolingdestinyr@gmail.com>
2021-01-28 04:37:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -172,8 +172,6 @@ def player_stats(sought_player_name)
       game_hash[place][attribute].each do |player|
         next unless player[:player_name] == sought_player_name
 
-      
-        end
       end
     end
   end
